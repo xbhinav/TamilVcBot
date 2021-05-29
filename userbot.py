721362432913e6003a349e6eabc86a173ca96adf
xbhinav <84801303+xbhinav@users.noreply.github.com>
2021-05-29 20:34:43 +0530

    Update userbot.py

--- userbot.py
+++ userbot.py
@@ -4,8 +4,8 @@ from pyrogram import Client, idle
 app = Client("tgvc")
 # logging.basicConfig(level=logging.INFO)
 app.start()
-print('>>> USERBOT STARTED by @TamilBots')
+print('>>> USERBOT STARTED by @Perfect_vazha')
 idle()
 app.stop()
-print('\n>>> USERBOT STOPPED by @TamilBots')
+print('\n>>> USERBOT STOPPED by @Perfect_vazha')
 
